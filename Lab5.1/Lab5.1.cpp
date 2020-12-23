d0e477614839061098dae6158f65b55468c6ba9d
cooll5 <52448498+cooll5@users.noreply.github.com>
2020-12-23 14:18:44 +0200

    first commit

---
+++ Lab5.1/Lab5.1.cpp
@@ -0,0 +1,19 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+using namespace std;
+double k(const double x, const double y); // прототип
+int main()
+{
+	double p, q;
+	cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+
+	double c = ((k^2*(1+p, q^2)- k*(q*p, 1))/(1+k*(p^2,q)));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double k(const double x, const double y) // визначення
+{
+	return sin(x) / (y * y) + cos(y) / x ^ 2;
+}
